92741d2d5ea68e972356ad2127bef0d267b049a4
Aniket uagre <ugareaniket28@gmail.com>
2024-07-11 23:59:41 +0530

    done with env

---
+++ chai-aur-javascript/save and work on github/app.js
@@ -0,0 +1,7 @@
+// git is version control system where we can track history of code 
+// and github is a platform of microsoft where developer can store and maintain their code 
+// there is option on github like codepace similar to vs code eidtor so we can use that to as a online env to code 
+//then you need to configure node js to run js code 
+//steps
+//1. view -> command pallet -> type add dev container configure files which is use to configure 
+//2.after that search node js and javascript environment.
---
+++ chai-aur-javascript/setting up environment/app.js
@@ -0,0 +1,7 @@
+// first install node js
+// node js is a platform or environment to run js outside browser.
+// deno is an another environment to run js 
+// we can run js file with command node app.js
+// js is extension of javascript file.
+
+console.log("chai aur code");
