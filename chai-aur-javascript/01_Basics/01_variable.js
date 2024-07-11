fef6f524eb475c054151960b0233d7243549caab
Aniket uagre <ugareaniket28@gmail.com>
2024-07-12 00:36:15 +0530

    done with variables and constant in js

---
+++ chai-aur-javascript/01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountNumber = 123456789; //it is constant type. 
+let accountName = "Aniket"; // this is block or function scope variable.
+var accountEmail = "Aniketu28@Gmail.com"; // this global variable.
+accountCity = "pune"; // we can decalre varible without let or var but not recommended to do this
+
+//accountNumber = 12345 // it not work we will not able to change constant.
+
+console.log(accountNumber);
+
+accountName ="ram"
+accountEmail = "Abc@gmail.com"
+accountCity = "mumbai"
+
+console.table([accountNumber,accountName,accountEmail,accountCity]);
+
+// special message = dont use var use always let because of block and function scope.
+
+
